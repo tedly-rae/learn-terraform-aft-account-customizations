a5084fb75214b76d2bf588a197b297c4a7b94202
Tan Ly <tientan0912@gmail.com>
2023-12-27 16:36:01 +0700

    remove s3 bucket

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,15 +1,15 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  # acl    = "private" (Deprecated)
-}
+# resource "aws_s3_bucket" "sandbox_bucket" {
+#   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+#   # acl    = "private" (Deprecated)
+# }
 
-resource "aws_s3_bucket_public_access_block" "block-sandbox-bucket" {
-  bucket = aws_s3_bucket.sandbox_bucket.id
+# resource "aws_s3_bucket_public_access_block" "block-sandbox-bucket" {
+#   bucket = aws_s3_bucket.sandbox_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
