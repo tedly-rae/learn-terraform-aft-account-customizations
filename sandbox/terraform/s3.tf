dd90c79d68d9255eef86cb0148c7d99d1f67f307
Tan Ly <tientan0912@gmail.com>
2023-12-27 15:18:25 +0700

    update new public access block

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,12 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   # acl    = "private" (Deprecated)
 }
+
+resource "aws_s3_bucket_public_access_block" "block-sandbox-bucket" {
+  bucket = aws_s3_bucket.sandbox_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
